8f4f7acc065ea868bd3af22f7b0268afef722dcb
Raza Awan <rzqutubawan@gmail.com>
2025-03-28 19:22:24 +0500

    Implement Web3Provider and layout components; integrate wallet connection functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,60 +1,19 @@
-"use client";
 
-import { Web3Provider } from "../Web3Provider";
-import { ConnectKitButton } from "connectkit";
-import Account from "./components/ui/client/Account";
-import Text from "./components/ui/common/text";
+'use client'
+import { Header, Footer, Main } from './components/layout';
 
 
-import styled from "styled-components";
-const StyledButton = styled.button`
-  cursor: pointer;
-  position: relative;
-  display: inline-block;
-  padding: 14px 24px;
-  color: #ffffff;
-  background: #1a88f8;
-  font-size: 16px;
-  font-weight: 500;
-  border-radius: 10rem;
-  box-shadow: 0 4px 24px -6px #1a88f8;
 
-  transition: 200ms ease;
-  &:hover {
-    transform: translateY(-6px);
-    box-shadow: 0 6px 40px -6px #1a88f8;
-  }
-  &:active {
-    transform: translateY(-3px);
-    box-shadow: 0 6px 32px -6px #1a88f8;
-  }
-`;
 
 export default function Home() {
   return (
-    <Web3Provider>
-      <main className="w-full min-h-screen flex  flex-col items-center justify-center p-10 gap-10">
-        <div className="max-w-5xl w-full flex items-center justify-center">
-          <Text variant="h1" color="default">
-            Welcome to Wagmi App
-          </Text>
-        </div>
-        <div className="max-w-5xl w-full flex items-center justify-center">
-        <ConnectKitButton.Custom>
-      {({ isConnected, show, truncatedAddress, ensName }) => {
-        return (
-          <StyledButton onClick={show}>
-            {isConnected ? ensName ?? 'Disconnect' : "Connect Wallet"}
-          </StyledButton>
-        );
-      }}
-    </ConnectKitButton.Custom>
-        </div>
-        <div className="max-w-5xl w-full flex items-center justify-center">
-        <Account />
-        </div>
-
-      </main>
-    </Web3Provider>
+    <div className="flex flex-col items-center justify-between p-0 min-h-screen bg-background">
+        <Header />
+        <main className="flex-1 container py-8">
+         <Main />
+        </main>
+        <Footer />
+     
+      </div>
   );
 }
