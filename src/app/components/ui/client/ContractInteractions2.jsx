78c6015cebca8345e4b594e5d96a6297160615df
Raza Awan <rzqutubawan@gmail.com>
2025-03-30 20:54:08 +0500

    Add ContractInteractions2 and ClientProviders components; create contractServices2.js file

---
+++ src/app/components/ui/client/ContractInteractions2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContractInteractions2 = () => {
+    return (
+        <div>
+            ContractInteractions2
+        </div>
+    );
+}
+
+export default ContractInteractions2;
---
+++ src/app/providers/ClientProviders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClientProviders = () => {
+    return (
+        <div>
+            ClientProviders
+        </div>
+    );
+}
+
+export default ClientProviders;
